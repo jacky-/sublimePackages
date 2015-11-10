affd600b3c8c1648b4a3426f0154474566091526
zheng_j <zheng_j@Ctrip.com>
2015-11-10 14:56:38 +0800

    new change

---
+++ BracketHighlighter/bh_modules/bashsupport.py
@@ -0,0 +1,28 @@
+"""
+BracketHighlighter.
+
+Copyright (c) 2013 - 2015 Isaac Muse <isaacmuse@gmail.com>
+License: MIT
+"""
+
+
+def validate(name, bracket, bracket_side, bfr):
+    """Check if bracket is lowercase."""
+
+    return bfr[bracket.begin:bracket.end].islower()
+
+
+def compare(name, first, second, bfr):
+    """Ensure correct open is paired with correct close."""
+
+    o = bfr[first.begin:first.end]
+    c = bfr[second.begin:second.end]
+
+    match = False
+    if o == "if" and c == "fi":
+        match = True
+    elif o in ["select", "for", "while", "until"] and c == "done":
+        match = True
+    elif o == "case" and c == "esac":
+        match = True
+    return match
